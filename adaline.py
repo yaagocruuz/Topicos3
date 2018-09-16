fb8c78f8160830796117f194d286c21cf1e7f4e5
Yago Cruz <yago.cruz@eloin.com>
2018-09-16 17:49:42 -0300

    lista 3

--- adaline.py
+++ adaline.py
@@ -4,41 +4,12 @@ import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 
 class AdalineGD(object):
-    """ADAptive LInear NEuron classifier.
-    Parameters
-    ------------
-    eta : float
-      Learning rate (between 0.0 and 1.0)
-    n_iter : int
-      Passes over the training dataset.
-    random_state : int
-      Random number generator seed for random weight
-      initialization.
-    Attributes
-    -----------
-    w_ : 1d-array
-      Weights after fitting.
-    cost_ : list
-      Sum-of-squares cost function value in each epoch.
-    """
     def __init__(self, eta=0.01, n_iter=50, random_state=1):
         self.eta = eta
         self.n_iter = n_iter
         self.random_state = random_state
 
     def fit(self, X, y):
-        """ Fit training data.
-        Parameters
-        ----------
-        X : {array-like}, shape = [n_samples, n_features]
-          Training vectors, where n_samples is the number of samples and
-          n_features is the number of features.
-        y : array-like, shape = [n_samples]
-          Target values.
-        Returns
-        -------
-        self : object
-        """
         rgen = np.random.RandomState(self.random_state)
         self.w_ = rgen.normal(loc=0.0, scale=0.01, size=1 + X.shape[1])
         self.cost_ = []
@@ -54,7 +25,7 @@ class AdalineGD(object):
             # a sigmoid function to implement a logistic regression classifier.
             output = self.activation(net_input)
             errors = (y - output)
-            self.w_[1:] += self.eta * X.T.dot(errors)
+            self.w_[1:] = self.w_[1:] + (self.eta * X.T.dot(errors)*1.0)
             self.w_[0] += self.eta * errors.sum()
             cost = (errors**2).sum() / 2.0
             self.cost_.append(cost)
@@ -71,31 +42,3 @@ class AdalineGD(object):
     def predict(self, X):
         """Return class label after unit step"""
         return np.where(self.activation(self.net_input(X)) >= 0.0, 1, -1)
-    
-    def plot_decision_regions(X, y, classifier, resolution=0.02):
-
-        # setup marker generator and color map
-        markers = ('s', 'x', 'o', '^', 'v')
-        colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
-        cmap = ListedColormap(colors[:len(np.unique(y))])
-
-        # plot the decision surface
-        x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
-        x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
-        xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
-                               np.arange(x2_min, x2_max, resolution))
-        Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
-        Z = Z.reshape(xx1.shape)
-        plt.contourf(xx1, xx2, Z, alpha=0.3, cmap=cmap)
-        plt.xlim(xx1.min(), xx1.max())
-        plt.ylim(xx2.min(), xx2.max())
-
-        # plot class samples
-        for idx, cl in enumerate(np.unique(y)):
-            plt.scatter(x=X[y == cl, 0], 
-                        y=X[y == cl, 1],
-                        alpha=0.8, 
-                        c=colors[idx],
-                        marker=markers[idx], 
-                        label=cl, 
-                        edgecolor='black')
--- perceptron.py
+++ perceptron.py
@@ -4,41 +4,13 @@ import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 
 class Perceptron(object):
-    """Perceptron classifier.
-    Parameters
-    ------------
-    eta : float
-      Learning rate (between 0.0 and 1.0)
-    n_iter : int
-      Passes over the training dataset.
-    random_state : int
-      Random number generator seed for random weight
-      initialization.
-    Attributes
-    -----------
-    w_ : 1d-array
-      Weights after fitting.
-    errors_ : list
-      Number of misclassifications (updates) in each epoch.
-    """
+    
     def __init__(self, eta=0.01, n_iter=50, random_state=1):
         self.eta = eta
         self.n_iter = n_iter
         self.random_state = random_state
 
     def fit(self, X, y):
-        """Fit training data.
-        Parameters
-        ----------
-        X : {array-like}, shape = [n_samples, n_features]
-          Training vectors, where n_samples is the number of samples and
-          n_features is the number of features.
-        y : array-like, shape = [n_samples]
-          Target values.
-        Returns
-        -------
-        self : object
-        """
         rgen = np.random.RandomState(self.random_state)
         self.w_ = rgen.normal(loc=0.0, scale=0.01, size=1 + X.shape[1])
         self.errors_ = []
@@ -47,44 +19,14 @@ class Perceptron(object):
             errors = 0
             for xi, target in zip(X, y):
                 update = self.eta * (target - self.predict(xi))
-                self.w_[1:] += update * xi
+                self.w_[1:] = self.w_[1:] + (update * (xi*1.0))
                 self.w_[0] += update
                 errors += int(update != 0.0)
             self.errors_.append(errors)
         return self
-
+    
     def net_input(self, X):
-        """Calculate net input"""
         return np.dot(X, self.w_[1:]) + self.w_[0]
 
     def predict(self, X):
-        """Return class label after unit step"""
         return np.where(self.net_input(X) >= 0.0, 1, -1)
-    
-    def plot_decision_regions(X, y, classifier, resolution=0.02):
-
-        # setup marker generator and color map
-        markers = ('s', 'x', 'o', '^', 'v')
-        colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
-        cmap = ListedColormap(colors[:len(np.unique(y))])
-
-        # plot the decision surface
-        x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
-        x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
-        xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
-                               np.arange(x2_min, x2_max, resolution))
-        Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
-        Z = Z.reshape(xx1.shape)
-        plt.contourf(xx1, xx2, Z, alpha=0.3, cmap=cmap)
-        plt.xlim(xx1.min(), xx1.max())
-        plt.ylim(xx2.min(), xx2.max())
-
-        # plot class samples
-        for idx, cl in enumerate(np.unique(y)):
-            plt.scatter(x=X[y == cl, 0], 
-                        y=X[y == cl, 1],
-                        alpha=0.8, 
-                        c=colors[idx],
-                        marker=markers[idx], 
-                        label=cl, 
-edgecolor='black')
